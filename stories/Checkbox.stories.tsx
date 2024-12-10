62a32af3aa259c980cb4930bcfda3c2794821bf3
Stijn Van Doorslaer <stijn@codeurs.be>
2024-12-10 14:39:31 +0100

    Checkbox layout

--- stories/Checkbox.stories.tsx
+++ stories/Checkbox.stories.tsx
@@ -2,8 +2,9 @@ import {Checkbox} from '../src/components/Checkbox'
 
 export const Example = () => (
   <div style={{display: 'flex', flexDirection: 'column', gap: 10}}>
-    <Checkbox>Unsubscribe</Checkbox>
-    <Checkbox isInvalid>Unsubscribe</Checkbox>
-    <Checkbox isDisabled>Unsubscribe</Checkbox>
+    <Checkbox>Checkbox</Checkbox>
+    <Checkbox isSelected>Checkbox isSelected</Checkbox>
+    <Checkbox isInvalid>Checkbox isInvalid</Checkbox>
+    <Checkbox isDisabled>Checkbox isDisabled</Checkbox>
   </div>
 )
