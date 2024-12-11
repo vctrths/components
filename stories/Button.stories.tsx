0751a4ace49dee5307d02d130b898af68e48fe78
Stijn Van Doorslaer <stijn@codeurs.be>
2024-12-11 17:03:51 +0100

    Button layout changes

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,4 +1,42 @@
+import type {CSSProperties} from 'react'
 import {Button} from '../src/components/Button.tsx'
+
+const styleStack: CSSProperties = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'flex-start',
+  gap: 10
+}
+
+export const All = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10
+      }}
+    >
+      <div style={styleStack}>
+        <Button>Solid</Button>
+        <Button isDisabled>Solid disabled</Button>
+        <Button appearance="outline">Outline</Button>
+        <Button appearance="outline" isDisabled>
+          Outline disabled
+        </Button>
+      </div>
+      <div style={styleStack}>
+        <Button intent="secondary">Solid secondary</Button>
+        <Button appearance="outline" intent="secondary">
+          Outline secondary
+        </Button>
+      </div>
+      <div style={styleStack}>
+        <Button appearance="plain">Plain</Button>
+      </div>
+    </div>
+  )
+}
 
 export const Appearance = () => {
   return (
