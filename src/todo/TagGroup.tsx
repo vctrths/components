996b40934acafea50b9a872d8847535d221c4a92
Stijn Van Doorslaer <stijn@codeurs.be>
2024-12-16 10:30:23 +0100

    TagGroup layout

--- src/todo/TagGroup.tsx
+++ src/todo/TagGroup.tsx
@@ -1,63 +1,63 @@
 import {
-  Button,
-  Label,
   Tag as AriaTag,
   TagGroup as AriaTagGroup,
-  TagGroupProps as AriaTagGroupProps,
+  type TagGroupProps as AriaTagGroupProps,
+  Button,
+  Label,
   TagList,
-  TagListProps,
-  TagProps,
+  type TagListProps,
+  type TagProps,
   Text
-} from 'react-aria-components';
+} from 'react-aria-components'
 
-import './TagGroup.css';
+import './TagGroup.css'
+
+export type IntentProps = 'primary' | 'secondary'
+export type ShapeProps = 'square' | 'circle'
 
 export interface TagGroupProps<T>
-  extends
-    Omit<AriaTagGroupProps, 'children'>,
+  extends Omit<AriaTagGroupProps, 'children'>,
     Pick<TagListProps<T>, 'items' | 'children' | 'renderEmptyState'> {
-  label?: string;
-  description?: string;
-  errorMessage?: string;
+  label?: string
+  description?: string
+  errorMessage?: string
+  intent?: 'primary' | 'secondary'
+  shape?: ShapeProps
 }
 
-export function TagGroup<T extends object>(
-  {
-    label,
-    description,
-    errorMessage,
-    items,
-    children,
-    renderEmptyState,
-    ...props
-  }: TagGroupProps<T>
-) {
+export function TagGroup<T extends object>({
+  label,
+  description,
+  errorMessage,
+  items,
+  children,
+  renderEmptyState,
+  intent,
+  shape,
+  ...props
+}: TagGroupProps<T>) {
   return (
-    (
-      <AriaTagGroup {...props}>
-        <Label>{label}</Label>
-        <TagList items={items} renderEmptyState={renderEmptyState}>
+    <AriaTagGroup data-intent={intent} data-shape={shape} {...props}>
+      <Label>{label}</Label>
+      <TagList items={items} renderEmptyState={renderEmptyState}>
+        {children}
+      </TagList>
+      {description && <Text slot="description">{description}</Text>}
+      {errorMessage && <Text slot="errorMessage">{errorMessage}</Text>}
+    </AriaTagGroup>
+  )
+}
+
+export function Tag({children, ...props}: TagProps) {
+  const textValue = typeof children === 'string' ? children : undefined
+  return (
+    <AriaTag textValue={textValue} {...props}>
+      {({allowsRemoving}) => (
+        <>
           {children}
-        </TagList>
-        {description && <Text slot="description">{description}</Text>}
-        {errorMessage && <Text slot="errorMessage">{errorMessage}</Text>}
-      </AriaTagGroup>
-    )
-  );
-}
-
-export function Tag({ children, ...props }: TagProps) {
-  let textValue = typeof children === 'string' ? children : undefined;
-  return (
-    (
-      <AriaTag textValue={textValue} {...props}>
-        {({ allowsRemoving }) => (
-          <>
-            {children}
-            {allowsRemoving && <Button slot="remove">ⓧ</Button>}
-          </>
-        )}
-      </AriaTag>
-    )
-  );
+          {allowsRemoving && <Button slot="remove">ⓧ</Button>}
+        </>
+      )}
+    </AriaTag>
+  )
 }
--- stories/TagGroup.stories.tsx
+++ stories/TagGroup.stories.tsx
@@ -1,15 +1,85 @@
-import {Tag, TagGroup} from '../src/todo/TagGroup.tsx'
+'use client'
 
-export const Example = (args: any) => (
-  <TagGroup {...args}>
-    <Tag>Chocolate</Tag>
-    <Tag>Mint</Tag>
-    <Tag>Strawberry</Tag>
-    <Tag>Vanilla</Tag>
-  </TagGroup>
+import {useListData} from 'react-stately'
+import {
+  type IntentProps,
+  type ShapeProps,
+  Tag,
+  TagGroup
+} from '../src/todo/TagGroup.tsx'
+
+const intents: IntentProps[] = ['primary', 'secondary']
+const shapes: ShapeProps[] = ['square', 'circle']
+
+const items = [
+  {id: 1, name: 'Chocolate'},
+  {id: 2, name: 'Mint'},
+  {id: 3, name: 'Strawberry'},
+  {id: 4, name: 'Vanilla'}
+]
+
+export const Intents = () => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 10
+    }}
+  >
+    {intents.map(intent => (
+      <TagGroup items={items} label={intent} intent={intent} key={intent}>
+        {item => <Tag>{item.name}</Tag>}
+      </TagGroup>
+    ))}
+  </div>
 )
 
-Example.args = {
-  label: 'Ice cream flavor',
-  selectionMode: 'single'
+export const Selection = () => {
+  const list = useListData({
+    initialItems: [
+      {id: 1, name: 'Chocolate'},
+      {id: 2, name: 'Mint'},
+      {id: 3, name: 'Strawberry'},
+      {id: 4, name: 'Vanilla'}
+    ]
+  })
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10
+      }}
+    >
+      <TagGroup items={list.items} label="Ice cream flavor">
+        {item => <Tag>{item.name}</Tag>}
+      </TagGroup>
+      <TagGroup
+        items={list.items}
+        label="Ice cream flavor"
+        description="Multiple selectionMode"
+        selectionMode="multiple"
+        onRemove={keys => list.remove(...keys)}
+      >
+        {item => <Tag>{item.name}</Tag>}
+      </TagGroup>
+    </div>
+  )
 }
+
+export const Shape = () => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 10
+    }}
+  >
+    {shapes.map(shape => (
+      <TagGroup items={items} label={shape} shape={shape} key={shape}>
+        {item => <Tag>{item.name}</Tag>}
+      </TagGroup>
+    ))}
+  </div>
+)
