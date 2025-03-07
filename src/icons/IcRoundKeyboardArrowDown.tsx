df552f8f2297369782da598abff506f3af875cba
Ben Merckx <ben@codeurs.be>
2025-03-07 11:17:56 +0100

    Consistent currentColor usage

--- src/icons/IcRoundKeyboardArrowDown.tsx
+++ src/icons/IcRoundKeyboardArrowDown.tsx
@@ -1,9 +1,6 @@
 import type {SVGProps} from 'react'
 
-export function IcRoundKeyboardArrowDown({
-  fill,
-  ...props
-}: SVGProps<SVGSVGElement> & {fill?: string}) {
+export function IcRoundKeyboardArrowDown(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -11,7 +8,7 @@ export function IcRoundKeyboardArrowDown({
       height="1em"
       display="block"
       viewBox="0 0 24 24"
-      fill={fill || 'currentColor'}
+      fill="currentColor"
       {...props}
     >
       <path d="M8.12 9.29L12 13.17l3.88-3.88a.996.996 0 1 1 1.41 1.41l-4.59 4.59a.996.996 0 0 1-1.41 0L6.7 10.7a.996.996 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0" />
