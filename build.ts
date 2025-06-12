34c4701636bd436b55007a452898836f3120c4bf
Ben Merckx <ben@codeurs.be>
2025-06-12 11:33:55 +0200

    Bundle licenses

--- build.ts
+++ build.ts
@@ -1,26 +1,49 @@
-import {relative} from 'node:path'
-import {Glob} from 'bun'
+import path from 'node:path'
+import {$, Glob} from 'bun'
 import {type Plugin, build} from 'esbuild'
+import fs from 'node:fs'
 
 const tsx = new Glob('src/**/*.tsx')
 const entries = [...tsx.scanSync()]
+const supportedLocales = new Set(['en-US'])
 
 const externalize: Plugin = {
   name: 'externalize',
   setup(build) {
-    build.onResolve({filter: /.*/}, ({path, ...args}) => {
+    Bun.write('dist/empty.js', '')
+    Bun.write('dist/empty.d.ts', '')
+    build.onResolve({filter: /.*/}, (args) => {
       if (args.kind === 'entry-point') return
-      if (path.includes('.css')) return
-      if (path.endsWith('.tsx') || path.endsWith('.ts'))
+      if (args.path.includes('.css')) return
+      if (args.path.endsWith('.tsx') || args.path.endsWith('.ts'))
         return {
-          path: `${path.slice(0, path.lastIndexOf('.'))}.js`,
+          path: `${args.path.slice(0, args.path.lastIndexOf('.'))}.js`,
           external: true
         }
-      return {path, external: true}
+      const base = path.basename(args.path, '.mjs')
+      if (base.match(/[a-z]{2}-[A-Z]{2}/)) {
+        if (!supportedLocales.has(base)) {
+          return {path: path.resolve('dist/empty.js')}
+        }
+      }
+      if (args.resolveDir.includes('node_modules')) {
+        const folder = args.resolveDir
+          .replaceAll('\\', '/')
+          .split('node_modules/')[1]
+        const segments = folder.split('/')
+        const module = folder.startsWith('@')
+          ? segments.slice(0, 2)
+          : segments.slice(0, 1)
+        modules.add(module.join('/'))
+      }
     })
     const cssFiles: Array<string> = []
-    build.onLoad({filter: /\.css$/}, ({path}) => {
-      cssFiles.push(relative(process.cwd(), path).replaceAll('\\', '/'))
+    let modules: Set<string>
+    build.onStart(() => {
+      modules = new Set()
+    })
+    build.onLoad({filter: /\.css$/}, (args) => {
+      cssFiles.push(path.relative(process.cwd(), args.path).replaceAll('\\', '/'))
       return {contents: ''}
     })
     build.onEnd(async () => {
@@ -36,16 +59,55 @@ const externalize: Plugin = {
         write: false
       })
       Bun.write('dist/index.css', res.outputFiles[0].text)
-      Bun.write('dist/empty.js', '')
-      Bun.write('dist/empty.d.ts', '')
+      const licenses = new Map<string, string>()
+      for (const module of modules) {
+        const target = path.join('node_modules', module)
+        const pkg = JSON.parse(
+          fs.readFileSync(path.join(target, 'package.json'), 'utf-8')
+        )
+        let licenseText: string = ''
+        try {
+          licenseText = fs.readFileSync(path.join(target, 'LICENSE'), 'utf-8')
+        } catch {}
+        if (!licenseText)
+          try {
+            licenseText = fs.readFileSync(
+              path.join(target, 'LICENSE.md'),
+              'utf-8'
+            )
+          } catch {}
+        if (pkg.license) licenseText = `# ${pkg.license}\n\n${licenseText}`
+        if (!licenseText) console.warn(`Missing license for module ${module}`)
+        else {
+          const prev = licenses.get(licenseText) ?? ''
+          const pkgs = `${prev}\n- ${module}@${pkg.version}`
+          licenses.set(licenseText, pkgs)
+        }
+      }
+      const output = Array.from(licenses.entries(), ([license, pkgs]) => {
+        return `${license}\n\nThis license applies to the following modules:\n${pkgs}`
+      }).join('\n\n---\n\n')
+      fs.writeFileSync(
+        path.join(build.initialOptions.outdir!, 'LICENSES.md'),
+        `This file contains the licenses of the bundled modules in this distribution.\n\n${output}`
+      )
     })
   }
 }
+
+await $`rm -rf dist`
 
 await build({
   format: 'esm',
   entryPoints: entries.filter(entry => !entry.includes('todo')),
   outdir: 'dist',
   bundle: true,
-  plugins: [externalize],
+  splitting: true,
+  plugins: [
+    externalize
+  ],
+  external: [
+    'react',
+    'react-dom'
+  ]
 }).catch(() => process.exit(1))
