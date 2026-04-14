30fa53a24181b4edb67d51ca312938264f19a7c3
vctrth <thysvic@gmail.com>
2026-04-14 11:42:13 +0200

    Updated popover stories

--- src/components/Popover.stories.tsx
+++ src/components/Popover.stories.tsx
@@ -1,15 +1,25 @@
-import {Button, DialogTrigger, Heading} from 'react-aria-components'
+import {DialogTrigger, Heading, TooltipProps} from 'react-aria-components'
 import {Popover} from './Popover.tsx'
+import { Button } from './Button.tsx'
+
+
+type Placement = Pick<TooltipProps, "placement">["placement"]
+const placements: Placement[] = ["bottom", "top", "left", "start", "right", "end"]
 
 export const Example = (args: any) => (
-  <DialogTrigger>
-    <Button aria-label="Help">ⓘ</Button>
-    <Popover {...args}>
-      <Heading slot="title">Help</Heading>
-      <p>For help accessing your account, please contact support.</p>
-    </Popover>
-  </DialogTrigger>
+ <div style={{marginTop: '256px', display: 'flex', gap: '256px', flexWrap: 'wrap'}}>
+  {placements.map((placement) => (
+    <DialogTrigger key={placement}>
+      <Button aria-label="Help">ⓘ</Button>
+      <Popover {...args} placement={placement} offset={24}>
+        <Heading slot="title">Popover</Heading>
+        <p>Showing popover at: {placement}</p>
+      </Popover>
+    </DialogTrigger>
+  ))} 
+  </ div>      
 )
+
 
 export default {
   title: 'Components / Popover'
